7bfaf4030aa20f3e1349a7e642fa6e5fe659881e
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-10 17:09:45 +0700

    add variable go

--- pertama/main.go
+++ pertama/main.go
@@ -1,15 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"pertama/calculation"
-)
+// FUNDAMENTAL
+// import (
+// 	"fmt"
+// 	"pertama/calculation"
+// )
 
-func main() {
-	fmt.Println("Halo, belajar golang")
+// func main() {
+// 	fmt.Println("Halo, belajar golang")
 
-	result := calculation.Add(1, 2)
+// 	result := calculation.Add(1, 2)
 
-	fmt.Println(result)
-	fmt.Println(calculation.Multiple(5, 22))
-}
+// 	fmt.Println(result)
+// 	fmt.Println(calculation.Multiple(5, 22))
+// }
+
+// VARIABLE
+// func main() {
+// 	var name string
+// 	name = "Ruby on Rails"
+// 	var number int
+// 	fmt.Println(number)
+// 	fmt.Println(name)
+// }
