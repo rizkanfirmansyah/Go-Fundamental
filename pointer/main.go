bc4369e9393ff5052e735419231a97d790e5ffc2
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-11 18:17:57 +0700

    add pointer on Go

--- pointer/main.go
+++ pointer/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
+	numberA := 5
+	numberB := &numberA
+
+	fmt.Println(numberA)
+	fmt.Println(numberB)
+	fmt.Println(*numberB)
+
+	*numberB = 10
+
+	fmt.Println(*numberB)
+	fmt.Println(numberA)
 
 }
