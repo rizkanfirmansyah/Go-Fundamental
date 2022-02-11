96030fb6be499cebd1ee1a84d3043954caed4dbc
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-11 17:06:24 +0700

    add struct on Go

--- pertama/main.go
+++ pertama/main.go
@@ -18,6 +18,15 @@ func main() {
 	user.LastName = "Firmansyah"
 	user.email = "rizkan@gmail.com"
 	user.IsActive = true
+
+	user2 := User{}
+	user2.ID = 2
+	user2.FirstName = "Ahmad"
+	user2.LastName = "Zulkifli"
+	user2.email = "ahmad@gmail.com"
+	user2.IsActive = true
+
 	fmt.Println(user)
+	fmt.Println(user2)
 
 }
