59bb6fee27bcea2fc998b086ce92af8555a9dbd5
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-11 17:08:18 +0700

    instance struct on {}

--- pertama/main.go
+++ pertama/main.go
@@ -12,21 +12,26 @@ type User struct {
 
 // Struct
 func main() {
-	user := User{}
-	user.ID = 1
-	user.FirstName = "Rizkan"
-	user.LastName = "Firmansyah"
-	user.email = "rizkan@gmail.com"
-	user.IsActive = true
+	user := User{
+		ID:        1,
+		FirstName: "Zelda",
+		LastName:  "Putri",
+		email:     "zelda@golang.go",
+		IsActive:  true,
+	}
+	// user.ID = 1
+	// user.FirstName = "Rizkan"
+	// user.LastName = "Firmansyah"
+	// user.email = "rizkan@gmail.com"
+	// user.IsActive = true
 
-	user2 := User{}
-	user2.ID = 2
-	user2.FirstName = "Ahmad"
-	user2.LastName = "Zulkifli"
-	user2.email = "ahmad@gmail.com"
-	user2.IsActive = true
+	// user2 := User{}
+	// user2.ID = 2
+	// user2.FirstName = "Ahmad"
+	// user2.LastName = "Zulkifli"
+	// user2.email = "ahmad@gmail.com"
+	// user2.IsActive = true
 
 	fmt.Println(user)
-	fmt.Println(user2)
 
 }
