39943d13f6d3bbca6d236d695ba52465c7365082
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-12 21:04:40 +0700

    new pointer

--- pointer/main.go
+++ pointer/main.go
@@ -3,16 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	numberA := 5
-	numberB := &numberA
+	// numberA := 5
+	// numberB := &numberA
+
+	// fmt.Println(numberA)
+	// fmt.Println(numberB)
+	// fmt.Println(*numberB)
+
+	// *numberB = 10
+
+	// fmt.Println(*numberB)
+	// fmt.Println(numberA)
+
+	var numberA int = 5
+	var numberB *int = &numberA
 
 	fmt.Println(numberA)
 	fmt.Println(numberB)
 	fmt.Println(*numberB)
 
-	*numberB = 10
-
-	fmt.Println(*numberB)
+	numberA = 20
 	fmt.Println(numberA)
+	fmt.Println(numberB)
 
 }
