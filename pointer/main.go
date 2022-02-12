4056dc2a72a334c1ccea7639aab1da75f8d48d36
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-12 21:36:23 +0700

    Function Struct with Interface

--- pointer/main.go
+++ pointer/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-type Luas interface {
+type BangunDatar interface {
 	HitungLuas() int
 }
 
@@ -23,12 +23,28 @@ func (persegiPanjang PersegiPanjang) HitungLuas() int {
 	return persegiPanjang.Panjang * persegiPanjang.Lebar
 }
 
-func main() {
-	bangunDatar := Persegi{Sisi: 4}
-	luas := SeberapaLuas(bangunDatar)
-	fmt.Println("Luas : ", luas)
+type Asal struct {
+	Angka int
 }
 
-func SeberapaLuas(bangunDatar Persegi) int {
+func (asal Asal) HitungLuas() int {
+	return 1001
+}
+
+func main() {
+	persegi := Persegi{Sisi: 4}
+	luas := SeberapaLuas(persegi)
+	fmt.Println("Luas Persegi : ", luas)
+
+	persegiPanjang := PersegiPanjang{Lebar: 4, Panjang: 6}
+	luas2 := SeberapaLuas(persegiPanjang)
+	fmt.Println("Luas Persegi Panjang: ", luas2)
+
+	asal := Asal{Angka: 9}
+	luasAsal := SeberapaLuas(asal)
+	fmt.Println("Luas Asal: ", luasAsal)
+}
+
+func SeberapaLuas(bangunDatar BangunDatar) int {
 	return bangunDatar.HitungLuas()
 }
