c67801a88ac38db012e41a1584e17405a119337e
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-12 21:10:20 +0700

    function pointer

--- pointer/main.go
+++ pointer/main.go
@@ -15,15 +15,27 @@ func main() {
 	// fmt.Println(*numberB)
 	// fmt.Println(numberA)
 
-	var numberA int = 5
-	var numberB *int = &numberA
+	// var numberA int = 5
+	// var numberB *int = &numberA
 
-	fmt.Println(numberA)
-	fmt.Println(numberB)
-	fmt.Println(*numberB)
+	// fmt.Println(numberA)
+	// fmt.Println(numberB)
+	// fmt.Println(*numberB)
 
-	numberA = 20
-	fmt.Println(numberA)
-	fmt.Println(numberB)
+	// numberA = 20
+	// fmt.Println(numberA)
+	// fmt.Println(numberB)
 
+	number := 5
+	fmt.Println("Alamat memory : ", &number)
+	fmt.Println("Nilai awal : ", number)
+	change(&number, 100)
+
+	fmt.Println("Nilai akhir : ", number)
+}
+
+func change(old *int, new int) {
+	*old = new
+	fmt.Println(old)
+	fmt.Println(old)
 }
